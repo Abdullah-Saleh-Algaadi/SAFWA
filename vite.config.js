a6e063d8c0c673aab5744b93decb6e1a846738c7
Abdullah Saleh <abdullahsalehali46@gmail.com>
2026-07-17 00:58:17 +0300

    Remove base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/SAFWA/',
   plugins: [react(), tailwindcss()],
   css: {
     postcss: {
